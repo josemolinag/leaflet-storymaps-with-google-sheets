1f0814197319149025315048d5c60dca88e0ea6a
josemolinag <josemolinagracia@gmail.com>
2019-05-05 17:42:41 +0200

    otro link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/166vqy55SmXO28q8RUN4V8-dS2KbWskuPQrZr0RsV8qc';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/166vqy55SmXO28q8RUN4V8-dS2KbWskuPQrZr0RsV8qc/edit#gid=1033046795';
