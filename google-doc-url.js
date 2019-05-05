d2e7aa7e2e62adeb068cd7af06040ce9dbca74c4
josemolinag <josemolinagracia@gmail.com>
2019-05-05 17:43:48 +0200

    update link
    link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/166vqy55SmXO28q8RUN4V8-dS2KbWskuPQrZr0RsV8qc/edit#gid=1033046795';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/166vqy55SmXO28q8RUN4V8-dS2KbWskuPQrZr0RsV8qc/edit#gid=0';
