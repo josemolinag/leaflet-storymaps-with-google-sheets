c3788ef20c957f6d30b86ef1cba2a190dd14a310
josemolinag <josemolinagracia@gmail.com>
2019-05-05 17:38:10 +0200

    actualizacion link spreadsheet

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT6CvoNgnc6QGTGxDPTo5GLuJG8HpcYC-_7z8lIyXQnjPq2IjBt-zTa5x0id5ufabvntmAtqOQYPDzX/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT6CvoNgnc6QGTGxDPTo5GLuJG8HpcYC-_7z8lIyXQnjPq2IjBt-zTa5x0id5ufabvntmAtqOQYPDzX';
